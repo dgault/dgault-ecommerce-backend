eb72aab56b088ae7c9c4519706bbb70d910635bf
David Gault <d.gault@dundee.ac.uk>
2022-08-05 05:31:01 +0100

    Create project for backend e-commerce API
    Initial project generated using https://start.spring.io/

---
+++ src/main/java/com/bcgdv/backend/WatchStore.java
@@ -0,0 +1,13 @@
+package com.bcgdv.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchStore {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchStore.class, args);
+	}
+
+}
---
+++ src/test/java/com/bcgdv/backend/WatchStoreIT.java
@@ -0,0 +1,13 @@
+package com.bcgdv.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchStoreIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
