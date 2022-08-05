4e57ec6a31e483147f3943af6220cae75c301441
David Gault <d.gault@dundee.ac.uk>
2022-08-05 07:58:52 +0100

    Implement WatchOrderPK and Discount
    Implementations of the WatchOrder Primary Key and Discount as required by the WatchOrder unit test.

--- src/main/java/com/bcgdv/backend/model/Discount.java
+++ src/main/java/com/bcgdv/backend/model/Discount.java
@@ -1,5 +1,49 @@
 package com.bcgdv.backend.model;
 
-public class Discount {
+import javax.persistence.Embeddable;
 
+@Embeddable
+public class Discount {
+  
+  public enum DiscountType {
+    NONE,
+    MULTI_BUY;
+  }
+
+  private String displayMessage = null;
+  private DiscountType type;
+  private Integer multiBuyQty = null;
+  private Double multiBuyPrice = null;
+
+  public Discount() {
+    this.type = DiscountType.NONE;
+  }
+
+  public Discount(String displayMessage) {
+    this.displayMessage = displayMessage;
+    this.type = DiscountType.NONE;
+  }
+  
+  public Discount(DiscountType type, String displayMessage, Integer multiBuyQty, Double multiBuyPrice) {
+    this.displayMessage = displayMessage;
+    this.type = type;
+    this.multiBuyQty = multiBuyQty;
+    this.multiBuyPrice = multiBuyPrice;
+  }
+
+  public String getDisplayMessage() {
+    return displayMessage;
+  }
+  
+  public DiscountType getType() {
+    return type;
+  }
+  
+  public Double getMultiBuyPrice() {
+    return multiBuyPrice;
+  }
+  
+  public Integer getMultiBuyQty() {
+    return multiBuyQty;
+  }
 }
--- src/main/java/com/bcgdv/backend/model/WatchOrderPK.java
+++ src/main/java/com/bcgdv/backend/model/WatchOrderPK.java
@@ -1,5 +1,91 @@
 package com.bcgdv.backend.model;
 
-public class WatchOrderPK {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "order")
+public class WatchOrderPK implements Serializable {
+
+  private static final long serialVersionUID = 36293518486154435L;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "order_id")
+  private Order order;
+
+  @ManyToOne(optional = false, fetch = FetchType.LAZY)
+  @JoinColumn(name = "watch_id")
+  private Watch watch;
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
+
+  public Watch getWatch() {
+    return watch;
+  }
+
+  public void setWatch(Watch watch) {
+    this.watch = watch;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+
+    result = prime * result + ((order.getId() == null)
+      ? 0
+      : order
+        .getId()
+        .hashCode());
+    result = prime * result + ((watch.getId() == null)
+      ? 0
+      : watch
+        .getId()
+        .hashCode());
+
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    WatchOrderPK other = (WatchOrderPK) obj;
+    if (order == null) {
+      if (other.order != null) {
+        return false;
+      }
+    } else if (!order.equals(other.order)) {
+      return false;
+    }
+
+    if (watch == null) {
+      if (other.watch != null) {
+        return false;
+      }
+    } else if (!watch.equals(other.watch)) {
+      return false;
+    }
+
+    return true;
+  }
 }
