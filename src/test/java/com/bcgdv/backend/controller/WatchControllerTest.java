783b756db885aa92f857a251d6b19e4c31213a0c
David Gault <d.gault@dundee.ac.uk>
2022-08-05 07:26:12 +0100

    Implement unit tests for Order and WatchController
    Add implementations for first model and controller unit tests.
    
    As the watch model will consist of only simple getters and setters I will be skipping the unit test due to the limited time for this project.

--- src/test/java/com/bcgdv/backend/controller/WatchControllerTest.java
+++ src/test/java/com/bcgdv/backend/controller/WatchControllerTest.java
@@ -2,13 +2,35 @@ package com.bcgdv.backend.controller;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import com.bcgdv.backend.model.Watch;
+import com.bcgdv.backend.service.WatchService;
 
 public class WatchControllerTest {
 
+  private WatchController watchController;
+
+  @Mock
+  private WatchService mockWatchService;
+  
+  @Mock
+  private Iterable<Watch> mockWatchList;
+  
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+    watchController = new WatchController(mockWatchService);
+  }
+ 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testGetWatches() {
+    Mockito.when(mockWatchService.getAllWatches()).thenReturn(mockWatchList);
+    assertEquals(mockWatchList, watchController.getWatches());
   }
 
 }
--- src/test/java/com/bcgdv/backend/model/OrderTest.java
+++ src/test/java/com/bcgdv/backend/model/OrderTest.java
@@ -2,13 +2,84 @@ package com.bcgdv.backend.model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 public class OrderTest {
+  
+  private static final double DELTA = 1e-15;
+  
+  Order order;
+  List<WatchOrder> watchList;
+  
+  @Mock
+  WatchOrder watchOrder1;
+  
+  @Mock
+  WatchOrder watchOrder2;
+  
+  @Mock
+  WatchOrder watchOrder3;
+
+  @Before
+  public void setup() {
+    order = new Order();
+    watchList = new ArrayList<WatchOrder>();
+    MockitoAnnotations.initMocks(this);
+    watchList.add(watchOrder1);
+    watchList.add(watchOrder2);
+    watchList.add(watchOrder3);
+    Mockito.when(watchOrder1.getTotalPrice()).thenReturn(100.50);
+    Mockito.when(watchOrder2.getTotalPrice()).thenReturn(50.00);
+    Mockito.when(watchOrder3.getTotalPrice()).thenReturn(25.25);
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void testGetTotalOrderPrice() {
+    assertEquals(0, order.getTotalOrderPrice().doubleValue(), DELTA);
+    order.setWatchOrders(watchList);
+    assertEquals(175.75, order.getTotalOrderPrice().doubleValue(), DELTA);
+  }
+  
+  @Test
+  public void testGetTotalOrderPrice_Null() {
+    order.setWatchOrders(null);
+    assertEquals(0, order.getTotalOrderPrice().doubleValue(), DELTA);
+  }
+  
+  @Test
+  public void testGetTotalOrderPrice_Overflow() {
+    Mockito.when(watchOrder1.getTotalPrice()).thenReturn(Double.MAX_VALUE);
+    order.setWatchOrders(watchList);
+    assertEquals(Double.MAX_VALUE, order.getTotalOrderPrice().doubleValue(), DELTA);
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testGetTotalOrderPrice_Negative() {
+    Mockito.when(watchOrder1.getTotalPrice()).thenReturn(-100.00);
+    order.setWatchOrders(watchList);
+    order.getTotalOrderPrice().doubleValue();
+  }
+
+  @Test
+  public void 
+  testGetNumberOfWatcheOrders()  {
+    assertEquals(0, order.getNumberOfWatcheOrders());
+    order.setWatchOrders(watchList);
+    assertEquals(3, order.getNumberOfWatcheOrders());
+  }
+  
+  @Test
+  public void 
+  testGetNumberOfWatcheOrders_Null()  {
+    order.setWatchOrders(null);
+    assertEquals(0, order.getNumberOfWatcheOrders());
   }
 
 }
